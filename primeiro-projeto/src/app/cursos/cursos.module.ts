7ccac5962421a2e8fcb8c107896f995ddb743906
nfandre <andre.freitas@ibm.com.br>
2020-02-17 17:52:53 -0300

    Aula 07 - introducao a servicos e injecao de dependencia

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import {CursosService} from "./cursos.service";
 
 
 
@@ -12,6 +13,7 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   ],
   exports: [
     CursosComponent
-  ]
+  ],
+  providers: [CursosService]
 })
 export class CursosModule { }
