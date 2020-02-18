f519442f3c2e48fdb5e94be94a9305ebdc19faee
nfandre <andre.freitas@ibm.com.br>
2020-02-18 11:44:21 -0300

    Aula 11 Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,11 @@ export class DataBindingComponent implements OnInit {
   url: string = 'www.nfandre.com';
   curso:boolean = false;
   urlImagem: string = 'https://st.depositphotos.com/1752371/1250/i/450/depositphotos_12507644-stock-photo-a-sword-with-a-red.jpg';
+
+  valorAtual: string = '';
+  valorSalvo: string;
+
+  isMouseOver: boolean = false;
   constructor() { }
 
   getCurtirCurso(){
@@ -18,6 +23,26 @@ export class DataBindingComponent implements OnInit {
   getValor(){
     return 1+2;
   }
+  botaoClicado(){
+    alert("Clicou");
+  }
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement> evento.target).value );
+    this.valorAtual =( <HTMLInputElement> evento.target).value;
+
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverUp(){
+    this.isMouseOver = true;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
   ngOnInit(): void {
   }
 
