95375ea841e4b542935667b278a8b2638d61bff4
nfandre <andre.freitas@ibm.com>
2020-06-26 14:09:05 -0300

    aula 50 - rotas simple

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from './home/home.component';
+import {CursosComponent} from './cursos/cursos.component';
+import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'cursos', component: CursosComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
